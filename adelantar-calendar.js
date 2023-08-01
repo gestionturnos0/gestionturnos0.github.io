3a2551cc7d9e70fcc5e14d0664595534354a4944
gestionturnos0 <132620457+gestionturnos0@users.noreply.github.com>
2023-08-01 12:28:53 -0300

    Update adelantar-calendar.js

--- adelantar-calendar.js
+++ adelantar-calendar.js
@@ -1,3 +1,3 @@
-let dia = new Date('2024-01-28T10:00:00');
+let dia = new Date('2023-09-01T10:00:00');
 $('#datetimepicker').data("DateTimePicker").date(dia);
 $('#datetimepicker').data("DateTimePicker").viewDate(dia);
