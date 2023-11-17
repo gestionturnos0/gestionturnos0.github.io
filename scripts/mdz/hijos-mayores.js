4e4937595ce858abc389b5921320ec61ed6eac0a
gestionturnos0 <132620457+gestionturnos0@users.noreply.github.com>
2023-11-17 19:06:25 -0300

    Add files via upload

---
+++ scripts/mdz/hijos-mayores.js
@@ -0,0 +1,461 @@
+const container = document.getElementsByTagName('section')[0];
+container.innerHTML = '';
+
+container.innerHTML = `
+<form action="https://prenotami.esteri.it/Services/Booking/435" enctype="multipart/form-data" id="bookingForm"
+    method="post">    
+    <div class="container" id="div-container">
+
+        <h1 class="heading-container" id="ServizioDescrizione">CITTADINANZA FIGLI DIRETTI MAGGIORENNI</h1>
+        <input id="hiddenServizioDesc" name="ServizioDescrizione" type="hidden"
+            value="CITTADINANZA FIGLI DIRETTI MAGGIORENNI">
+        <input data-val="true" data-val-required="The MessaggioRassicuranteWaitingList field is required."
+            id="MessaggioRassicuranteWaitingList" name="MessaggioRassicuranteWaitingList" type="hidden" value="False">
+        <input data-val="true" data-val-required="The isWaitingListEnabled field is required." id="isWaitingListEnabled"
+            name="isWaitingListEnabled" type="hidden" value="False">
+
+
+        <div id="divRiepilogoPrenotazione" class="riepilogoPrenotazione" style="">
+            <div id="divTitolo"></div>
+            <div id="divDettaglio"></div>
+            <div id="divAppuntamenti">
+                <ul>
+                    <li></li>
+                </ul>
+            </div>
+        </div>
+        <div>
+            <h2>Informazioni sulla prenotazione</h2>
+
+            <div id="typeofbooking">
+                <input data-val="true" data-val-number="Il campo IDServizioConsolare deve essere un numero."
+                    data-val-required="The IDServizioConsolare field is required." id="IDServizioConsolare"
+                    name="IDServizioConsolare" type="hidden" value="27">
+                <input data-val="true" data-val-number="Il campo IDServizioErogato deve essere un numero."
+                    data-val-required="The IDServizioErogato field is required." id="IDServizioErogato"
+                    name="IDServizioErogato" type="hidden" value="435">
+                <label>Tipo Prenotazione</label>
+                <select id="typeofbookingddl">
+                    <option value="1">Prenotazione Singola</option>
+                </select>
+                <input data-val="true" data-val-number="Il campo IdTipoPrenotazione deve essere un numero."
+                    data-val-required="The IdTipoPrenotazione field is required." id="hiddenTipoPrenotazione"
+                    name="IdTipoPrenotazione" type="hidden" value="1">
+                <input data-val="true" data-val-number="Il campo NumMaxAccompagnatori deve essere un numero."
+                    data-val-required="The NumMaxAccompagnatori field is required." id="hiddenNumMax"
+                    name="NumMaxAccompagnatori" type="hidden" value="0">
+
+
+
+            </div>
+            <div id="numberOfCompanions" style="display:none">
+                <label>Numero richiedenti aggiuntivi</label>
+                <select id="ddlnumberofcompanions">
+                </select>
+                <input data-val="true" data-val-number="Il campo NumAccompagnatoriSelected deve essere un numero."
+                    data-val-required="The NumAccompagnatoriSelected field is required." id="numAccSelected"
+                    name="NumAccompagnatoriSelected" type="hidden" value="0">
+            </div>
+
+            <div id="infoapplicant" style="display:none">
+                <h2>Dati Richiedente</h2>
+            </div>
+
+            <div id="ifMultiple" style="display: none;">
+            </div>
+        </div>
+        <div>
+            <h2>Note per la sede</h2>
+            <textarea cols="20" id="BookingNotes" maxlength="100" name="BookingNotes" rows="2"
+                style="width:100%; height: 200px; resize:none"></textarea>
+        </div>
+
+        <div style="margin:30px 0px 30px 0px;">
+            <div id="IdOtpSent" style="display:none; color:green;">
+                <p></p>
+            </div>
+            <div id="IdOtpInvalid" style="display: none; color: red;">
+                <p></p>
+            </div>
+
+        </div>
+
+        <div>
+            <h2>Informativa sulla Privacy</h2>
+            <div>
+                <div class="row">
+                    <input data-val="true" data-val-required="The PrivacyCheck field is required." id="PrivacyCheck"
+                        name="PrivacyCheck" required=""
+                        style="-ms-transform: scale(2); -webkit-transform: scale(2); width:50px;" type="checkbox"
+                        value="true"><input name="PrivacyCheck" type="hidden" value="false">
+                    <label style="font-size:large">Ho preso visione e accetto l <a
+                            href="https://prenotami.esteri.it/Content/docs/PRIVACY_LLTP.pdf#toolbar=0&amp;zoom=141"
+                            target="_blank"> Informativa per la privacy</a></label>
+                    <div>
+                        <br>
+                        <span class="field-validation-valid" data-valmsg-for="Privacy" data-valmsg-replace="true"
+                            style="text-align:left; color:red;"></span>
+                        <span tipoprenotazionegiainserito="Tipo di prenotazione già effettuata per questo servizio"
+                            class="field-validation-valid" data-valmsg-for="Invalid booking" data-valmsg-replace="true"
+                            style="text-align:left; color:red;"></span>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="footing-container">
+            <button type="button" class="button primary" onclick="window.location.href=&#39;/Services/Index&#39;;">Torna
+                alla lista</button>
+            <button type="submit" id="btnAvanti" class="button primary g-recaptcha"
+                data-sitekey="6LdSmG4cAAAAAOarRxGIhehvv4sPgVeF-vRi-Jqb" data-callback="onSubmit">Avanti</button>
+        </div>
+    </div>
+</form>
+`;
+
+//Scripts...
+var numDivCompanionsVisibile = 0;
+//Do not wait to load
+//$(document).ready(function () {
+$('#ServizioDescrizione').text($('#hiddenServizioDesc').val());
+getTypeofBooking();
+ShowInfoApplicant();
+getDatiAddizionali();
+
+$('#ddlnumberofcompanions').val(0);
+var x = 0;
+if (x !== 0) {
+    CreateDivCompanion(x);
+}
+
+//}); //Do not wait to load
+
+function aggiornaDettaglioPrenotazione() {
+    $('#divRiepilogoPrenotazione').hide();
+
+    aggiornaElencoPrenotanti();
+
+    $('#divRiepilogoPrenotazione').slideDown(1000);
+}
+
+function aggiornaElencoPrenotanti() {
+    let name = '';
+    $('figure.main-nav__avatar').each(function( key, value ) {
+        name = $(this).find('figcaption').html();
+    });
+    console.log(name);
+
+    $('#divRiepilogoPrenotazione > #divTitolo').text($("#typeofbookingddl option:selected").text());
+    switch ($('#typeofbookingddl').val()) {
+        case '1':
+            $('#divRiepilogoPrenotazione > #divDettaglio').html("\n\n  Stai prenotando per   " + 1 + " Appuntamento");
+
+
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').empty();
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').append('\n<li>' + name + '</li>');
+
+            break;
+        case '2':
+            var numapp = parseInt($('#ddlnumberofcompanions').val()) + 1;
+            if (numapp == 1) {
+                $('#divRiepilogoPrenotazione > #divDettaglio').html("\n\n Stai prenotando per  " + numapp + " Appuntamento");
+
+            } else {
+                $('#divRiepilogoPrenotazione > #divDettaglio').html("\n\n Stai prenotando per  " + numapp + " Appuntamenti");
+
+            }
+
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').empty();
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').append('\n<li>' + name + '</li>');
+            for (var i = 0; i < parseInt($('#ddlnumberofcompanions').val()); i++) {
+                $('#divRiepilogoPrenotazione > #divAppuntamenti ul').append('\n<li>' + $('#Accompagnatori_' + i + '__CognomeAccompagnatore').val() + ' ' + $('#Accompagnatori_' + i + '__NomeAccompagnatore').val() + '</li>');
+            }
+
+            break;
+            //Model.PrenotazioneSenzaRichiedente &&
+        case '3':
+            var numapp = 1;
+            if (numapp == 1) {
+                $('#divRiepilogoPrenotazione > #divDettaglio').html("\n\n Stai prenotando per  " + numapp + " Appuntamento");
+
+            } else {
+                $('#divRiepilogoPrenotazione > #divDettaglio').html("\n\n Stai prenotando per  " + numapp + " Appuntamenti");
+
+            }
+
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').empty();
+
+            $('#divRiepilogoPrenotazione > #divAppuntamenti ul').append('\n<li>' + $('#RichiedenteFittizioCognome').val() + ' ' + $('#RichiedenteFittizioNome').val() + '</li>')
+
+            break;
+    }
+}
+
+function ShowInfoApplicant() {
+    if ($('#infoapplicant').children().length > 1) {
+        $('#infoapplicant').show();
+    }
+}
+var _datiAddizionaliAccompagnatoriCreati = false;
+
+function ShowCompanions() {
+
+    $('#divRichiedenteFittizio').hide();
+    $('#infoapplicant').hide();
+
+    var selected = $('#typeofbookingddl').val();
+    if (selected === '1') {
+        $('#numberOfCompanions').hide();
+        $('#ifMultiple').hide();
+        $('#numAccSelected').val(0);
+        ShowInfoApplicant();
+    }
+    if (selected === '2') {
+        if (_datiAddizionaliAccompagnatoriCreati == false) {
+            GetDatiAddizionaliAccompagnatori();
+            _datiAddizionaliAccompagnatoriCreati = true;
+        }
+        $('#numberOfCompanions').show();
+        var x = 0;
+        if (x !== 0) {
+            $('#ddlnumberofcompanions').val(x);
+            CreateDivCompanion(x);
+        } else {
+            $('#ddlnumberofcompanions').val("1");
+            CreateDivCompanion(1);
+        }
+        ShowInfoApplicant();
+    }
+
+    if (selected === '3') {
+
+        //show FORM RICHIEDENTE
+        ShowInfoApplicant();
+
+        $('#divRichiedenteFittizio').show();
+        ShowInfoApplicant();
+
+    }
+    $('#hiddenTipoPrenotazione').val(selected);
+}
+
+function CreateDivCompanion(d) {
+    for (i = 0; i < d; i++) {
+        let div = document.querySelector('#divCompanion_' + i);
+        div.style.display = 'block';
+
+        if (d >= numDivCompanionsVisibile) {
+            for (let i = d - numDivCompanionsVisibile; i < d; i++) {
+                let div = document.querySelector('#divCompanion_' + i);
+                div.style.display = 'block';
+            }
+        } else {
+            for (let i = numDivCompanionsVisibile - 1; i >= d; i--) {
+                let div = document.querySelector('#divCompanion_' + i);
+                div.style.display = 'none';
+            }
+        }
+        $('#ifMultiple').show();
+
+        for (let i = 0; i < d; i++) {
+            let div = document.querySelector('#divCompanion_' + i);
+            div.style.display = 'block';
+            let divrel = $('#ddlRelation_' + i);
+            divrel.empty();
+            divrel.append($('<select/>', {
+                id: "TypeOfRelationDDL" + i,
+                'data-index': i
+            }));
+            ddl = $('#TypeOfRelationDDL' + i)
+            getRelationshipType(ddl, i);
+            ddl.change(function () {
+                let index = $(this).data('index');
+                let selected = $('#TypeOfRelationDDL' + index).val();
+                $('#hiddenParentela_' + index).val(selected);
+            });
+        }
+        numDivCompanionsVisibile = d;
+        $('#numAccSelected').val(d);
+    }
+}
+
+function getTypeofBooking() {
+    let bookingTypes = [
+        {
+            "Disabled": false,
+            "Group": null,
+            "Selected": false,
+            "Text": "Reserva unica",
+            "Value": "1"
+        }, {
+            "Disabled": false,
+            "Group": null,
+            "Selected": false,
+            "Text": "Reserva multiple",
+            "Value": "2"
+        }
+    ];
+
+    var numMax = $('#hiddenNumMax').val();
+    if (numMax !== "0") {
+        for (i = 0; i < bookingTypes.length; i++) {
+            $('#typeofbookingddl').append($('<option/>', {
+                value: bookingTypes[i].Value,
+                text: bookingTypes[i].Text
+            }));
+        }
+    } else {
+        $('#typeofbookingddl').append($('<option/>', {
+            value: bookingTypes[0].Value,
+            text: bookingTypes[0].Text
+        }));
+    }
+    if (0 === 0) {
+        $('#typeofbookingddl').val(1);
+    } else {
+        $('#typeofbookingddl').val(0);
+    }
+    ShowCompanions();
+    aggiornaDettaglioPrenotazione();
+}
+
+function getRelationshipType(ddl, index) {
+    $.getJSON("/Services/GetRelationshipType", function (data) {
+        for (i = 0; i < data.length; i++) {
+            ddl.append($('<option/>', {
+                value: data[i].Value,
+                text: data[i].Text
+            }));
+            var x = $('#hiddenParentela_' + index).val();
+            ddl.val(x);
+        }
+    })
+}
+
+function getDatiAddizionali() {
+    let RegEx;
+    let MaxLength;
+}
+
+function GetDatiAddizionaliAccompagnatori() {
+    let RegEx;
+    let MaxLength;
+}
+
+function getDropDownListsValues(_id, ddl, selectedValue) {
+    $.getJSON("/Services/GetDdlValuesByID/", {
+        id: _id
+    }, function (data) {
+        for (i = 0; i < data.length; i++) {
+            ddl.append($('<option/>', {
+                value: data[i].Value,
+                text: data[i].Text
+            }));
+        }
+        ddl.val(selectedValue);
+    })
+};
+
+function saveDdlValue() {};
+
+function CheckMaxFileSize() {
+    let result = true;
+    $('input[type="file"]').each(function () {
+        if (result) {
+            let _f = $(this)[0];
+            if (_f.files.length > 0) {
+                if (_f.files[0].size > 1024 * 1024 * 1) {
+                    alert('File troppo grande');
+                    result = false;
+                }
+            }
+        }
+
+    });
+    return result;
+}
+$('#typeofbookingddl').change(function () {
+    ShowCompanions();
+    aggiornaDettaglioPrenotazione();
+});
+$('#ddlnumberofcompanions').change(function () {
+    var n = $('#ddlnumberofcompanions').val();
+    CreateDivCompanion(n);
+    aggiornaDettaglioPrenotazione();
+});
+$(function () {
+    $("#btnAvanti").click(function () {
+
+
+        if (!CheckMaxFileSize()) {
+            event.preventDefault();
+            return;
+        }
+
+        saveDdlValue();
+
+
+
+        if (window.confirm("Si vuole confermare questo appuntamento? \n\n" + $('#divRiepilogoPrenotazione').text())) {
+
+        } else {
+            event.preventDefault();
+            return;
+        }
+
+
+
+
+
+
+
+
+    });
+
+});
+
+
+
+function controlloFileCaricato(file) {
+    id = file.id.split("_");
+    id1 = id[1];
+    if (id.length > 2) {
+        id2 = id[4];
+        validator = $('span[data-valmsg-for="Required_' + id1 + '_' + id2 + '"]');
+    } else {
+        validator = $('span[data-valmsg-for="Required_' + id1 + '"]');
+    }
+    if (file.files.length > 0) {
+        validator.hide();
+    }
+}
+
+function ControlloTesto(text) {
+    id = text.id.split("_");
+    id1 = id[1];
+    if (id.length > 2) {
+        id2 = id[4];
+        validator = $('span[data-valmsg-for="addizionale_' + id1 + '_' + id2 + '"]');
+    } else {
+        validator = $('span[data-valmsg-for="addizionale_' + id1 + '"]');
+    }
+    if (text != " ") {
+        validator.hide();
+    }
+
+}
+
+function ControloSelect(select) {
+    id = select.id.split("_");
+    id1 = id[1];
+    if (id.length > 2) {
+        id2 = id[4];
+        validator = $('span[data-valmsg-for="addizionale_' + id1 + '_' + id2 + '"]');
+    } else {
+        validator = $('span[data-valmsg-for="addizionale_' + id1 + '"]');
+    }
+    if (select.selectedIndex != 0) {
+        validator.hide();
+    }
+
+}
