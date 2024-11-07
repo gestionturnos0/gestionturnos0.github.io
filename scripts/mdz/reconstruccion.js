9607a05b5030543bb032a399854d67a2cebb9a43
gestionturnos0 <132620457+gestionturnos0@users.noreply.github.com>
2024-11-07 19:05:12 -0300

    Update reconstruccion.js

--- scripts/mdz/reconstruccion.js
+++ scripts/mdz/reconstruccion.js
@@ -2,23 +2,29 @@ const container = document.getElementsByTagName('section')[0];
 container.innerHTML = '';
 
 container.innerHTML = `
-<form action="/Services/Booking/5291" enctype="multipart/form-data" id="bookingForm" method="post">    
+<form action="https://prenotami.esteri.it/Services/Booking/5291" enctype="multipart/form-data" id="bookingForm"
+    method="post">    
     <div class="container" id="div-container">
 
-        <h1 class="heading-container" id="ServizioDescrizione"></h1>
+        <h1 class="heading-container" id="ServizioDescrizione">Servizio trattazione pratiche cittadinanza per
+            discendenza</h1>
         <input id="hiddenServizioDesc" name="ServizioDescrizione" type="hidden"
-            value="Servizio trattazione pratiche cittadinanza per discendenza" />
+            value="Servizio trattazione pratiche cittadinanza per discendenza">
         <input data-val="true" data-val-required="The MessaggioRassicuranteWaitingList field is required."
-            id="MessaggioRassicuranteWaitingList" name="MessaggioRassicuranteWaitingList" type="hidden" value="False" />
+            id="MessaggioRassicuranteWaitingList" name="MessaggioRassicuranteWaitingList" type="hidden" value="True">
         <input data-val="true" data-val-required="The isWaitingListEnabled field is required." id="isWaitingListEnabled"
-            name="isWaitingListEnabled" type="hidden" value="False" />
+            name="isWaitingListEnabled" type="hidden" value="False">
 
 
-        <div id="divRiepilogoPrenotazione" class="riepilogoPrenotazione" style="display:none;">
-            <div id="divTitolo"></div>
-            <div id="divDettaglio"></div>
+        <div id="divRiepilogoPrenotazione" class="riepilogoPrenotazione" style="">
+            <div id="divTitolo">Prenotazione Singola</div>
+            <div id="divDettaglio">
+
+                Stai prenotando per 1 Appuntamento</div>
             <div id="divAppuntamenti">
-                <ul></ul>
+                <ul>
+                    <li></li>
+                </ul>
             </div>
         </div>
         <div>
@@ -27,18 +33,20 @@ container.innerHTML = `
             <div id="typeofbooking">
                 <input data-val="true" data-val-number="Il campo IDServizioConsolare deve essere un numero."
                     data-val-required="The IDServizioConsolare field is required." id="IDServizioConsolare"
-                    name="IDServizioConsolare" type="hidden" value="27" />
+                    name="IDServizioConsolare" type="hidden" value="27">
                 <input data-val="true" data-val-number="Il campo IDServizioErogato deve essere un numero."
                     data-val-required="The IDServizioErogato field is required." id="IDServizioErogato"
-                    name="IDServizioErogato" type="hidden" value="5291" />
+                    name="IDServizioErogato" type="hidden" value="5291">
                 <label>Tipo Prenotazione</label>
-                <select id="typeofbookingddl"></select>
+                <select id="typeofbookingddl">
+                    <option value="1">Prenotazione Singola</option>
+                </select>
                 <input data-val="true" data-val-number="Il campo IdTipoPrenotazione deve essere un numero."
                     data-val-required="The IdTipoPrenotazione field is required." id="hiddenTipoPrenotazione"
-                    name="IdTipoPrenotazione" type="hidden" value="1" />
+                    name="IdTipoPrenotazione" type="hidden" value="1">
                 <input data-val="true" data-val-number="Il campo NumMaxAccompagnatori deve essere un numero."
                     data-val-required="The NumMaxAccompagnatori field is required." id="hiddenNumMax"
-                    name="NumMaxAccompagnatori" type="hidden" value="0" />
+                    name="NumMaxAccompagnatori" type="hidden" value="0">
 
 
 
@@ -49,35 +57,89 @@ container.innerHTML = `
                 </select>
                 <input data-val="true" data-val-number="Il campo NumAccompagnatoriSelected deve essere un numero."
                     data-val-required="The NumAccompagnatoriSelected field is required." id="numAccSelected"
-                    name="NumAccompagnatoriSelected" type="hidden" value="0" />
+                    name="NumAccompagnatoriSelected" type="hidden" value="0">
             </div>
 
-            <div id="applicantBufferDiv" style="margin:10px 0px 30px 0px;">
+            <div id="applicantBufferDiv" style="margin: 10px 0px 30px; display: none;">
                 <h2>Dati Richiedente</h2>
                 <p><strong>Cognome</strong></p>
                 <p><strong>Nome</strong></p>
                 <p><strong>Data di nascita</strong></p>
             </div>
 
-            <div id="infoapplicant" style="display:none">
+            <div id="infoapplicant" style="">
+                <div id="datoaddizionale_0">
+                    <label id="addizionale">
+                        Nome dell'avo italiano o del parente già cittadino e registrato in anagrafe Consolare
+                        &nbsp;<b>*</b> </label>
+                    <input id="DatiAddizionaliPrenotante_0___Descrizione"
+                        name="DatiAddizionaliPrenotante[0]._Descrizione" type="hidden"
+                        value="Nome dell&#39;avo italiano o del parente già cittadino e registrato in anagrafe Consolare">
+                    <input id="DatiAddizionaliPrenotante_0___testo" maxlength="100"
+                        name="DatiAddizionaliPrenotante[0]._testo" onchange="ControlloTesto(this)" type="text"
+                        value=""><input data-val="true" data-val-required="The _Obbligatorio field is required."
+                        id="DatiAddizionaliPrenotante_0___Obbligatorio"
+                        name="DatiAddizionaliPrenotante[0]._Obbligatorio" type="hidden" value="True">
+                    <div>
+                        <p><span class="field-validation-valid" data-valmsg-for="addizionale_0"
+                                data-valmsg-replace="true" style="color:red"></span></p>
+                        <p><span class="field-validation-valid" data-valmsg-for="addizionaleRegEx_0"
+                                data-valmsg-replace="true" style="color:red"></span></p>
+                    </div>
+                </div>
+                <input data-val="true" data-val-number="Il campo _Id deve essere un numero."
+                    data-val-required="The _Id field is required." id="hiddenId0"
+                    name="DatiAddizionaliPrenotante[0]._Id" type="hidden" value="68012"><input data-val="true"
+                    data-val-number="Il campo IDTipoDatoAddizionale deve essere un numero."
+                    data-val-required="The IDTipoDatoAddizionale field is required." id="hiddenIdDatoADD_0"
+                    name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.IDTipoDatoAddizionale" type="hidden"
+                    value="60"><input data-val="true" data-val-number="Il campo IDTipoControllo deve essere un numero."
+                    data-val-required="The IDTipoControllo field is required." id="hiddenCodDatoAdd_0"
+                    name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.IDTipoControllo" type="hidden"
+                    value="2"><input id="hiddenRegEx_0" name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.RegEx"
+                    type="hidden" value="">
+                <div id="docAddizionale_0">
+                    <label id="doc">
+                        Documento di identità/viaggio &nbsp;<b>*</b> </label>
+                    <p class="helper">E' possibile inserire file in formato PDF di massimo un 1MB<b>;</b></p>
+                    <input id="DocumentiPrenotante_0___Descrizione" name="DocumentiPrenotante[0]._Descrizione"
+                        type="hidden" value="Documento di identità/viaggio">
+                    <input id="File_0" name="DocumentiPrenotante[0]._File" onchange="controlloFileCaricato(this)"
+                        type="file" value="">
+                    <span class="field-validation-valid" data-valmsg-for="Lenght_0" data-valmsg-replace="true"
+                        style="color:red"></span>
+                    <input data-val="true" data-val-required="The _Obbligatorio field is required."
+                        id="DocumentiPrenotante_0___Obbligatorio" name="DocumentiPrenotante[0]._Obbligatorio"
+                        type="hidden" value="True">
+                    <span class="field-validation-valid validator-required" data-valmsg-for="Required_0"
+                        data-valmsg-replace="true" style="color:red"></span>
+                    <span class="field-validation-valid validator-required" data-valmsg-for="Extension_0"
+                        data-valmsg-replace="true" style="color:red"></span>
+                    <input data-val="true" data-val-number="Il campo _Id deve essere un numero."
+                        data-val-required="The _Id field is required." id="DocumentiPrenotante_0___Id"
+                        name="DocumentiPrenotante[0]._Id" type="hidden" value="15835">
+                </div>
+                <input data-val="true" data-val-number="Il campo IDTipoDocumento deve essere un numero."
+                    data-val-required="The IDTipoDocumento field is required." id="hiddenIdDocADD_0"
+                    name="DocumentiPrenotante[0]._TipoDocumento.IDTipoDocumento" type="hidden" value="4"><input
+                    id="hiddenFile_0" name="DocumentiPrenotante[0]._File" type="hidden" value="">
             </div>
 
-            <div id="ifMultiple">
+            <div id="ifMultiple" style="display: none;">
             </div>
         </div>
         <div>
             <h2>Note per la sede</h2>
             <textarea cols="20" id="BookingNotes" maxlength="100" name="BookingNotes" rows="2"
-                style="width:100%; height: 200px; resize:none">
-</textarea>
+                style="width:100%; height: 200px; resize:none"></textarea>
         </div>
 
         <div style="margin:30px 0px 30px 0px;">
             <h2>OTP</h2>
             <input type="text" placeholder="OTP" id="otp-input" name="otp-input" class="name form-control"
-                style="margin-bottom:20px;" required />
+                style="margin-bottom:20px;" required="">
             <button type="button" class="button primary" id="otp-send" onclick="sendOTP();">Invia nuovo codice</button>
-            <img id="otp-loader" src="/Content/img/loading.GIF" alt=""
+            <img id="otp-loader" src="./...._files/loading.GIF" alt=""
                 style="height:50px;margin-left:20px;display:none;">
             <div id="IdOtpSent" style="display:none; color:green;">
                 <p>Nuovo codice inviato!</p>
@@ -85,9 +147,6 @@ container.innerHTML = `
             <div id="IdOtpInvalid" style="display: none; color: red;">
                 <p>Codice errato!</p>
             </div>
-            <script>
-                window.alert('Codice errato!');
-            </script>
 
         </div>
 
@@ -95,18 +154,18 @@ container.innerHTML = `
             <h2>Informativa sulla Privacy</h2>
             <div>
                 <div class="row">
-                    <input checked="checked" data-val="true" data-val-required="The PrivacyCheck field is required."
-                        id="PrivacyCheck" name="PrivacyCheck" required=""
+                    <input data-val="true" data-val-required="The PrivacyCheck field is required." id="PrivacyCheck"
+                        name="PrivacyCheck" required=""
                         style="-ms-transform: scale(2); -webkit-transform: scale(2); width:50px;" type="checkbox"
-                        value="true" /><input name="PrivacyCheck" type="hidden" value="false" />
+                        value="true"><input name="PrivacyCheck" type="hidden" value="false">
                     <label style="font-size:large">Ho preso visione e accetto l <a
-                            href="/Content/docs/PRIVACY_LLTP.pdf#toolbar=0&amp;zoom=141" target="_blank"> Informativa
-                            per la privacy</a></label>
+                            href="https://prenotami.esteri.it/Content/docs/PRIVACY_LLTP.pdf#toolbar=0&amp;zoom=141"
+                            target="_blank"> Informativa per la privacy</a></label>
                     <div>
-                        <br />
+                        <br>
                         <span class="field-validation-valid" data-valmsg-for="Privacy" data-valmsg-replace="true"
                             style="text-align:left; color:red;"></span>
-                        <span TipoPrenotazioneGiaInserito="Tipo di prenotazione già effettuata per questo servizio"
+                        <span tipoprenotazionegiainserito="Tipo di prenotazione già effettuata per questo servizio"
                             class="field-validation-valid" data-valmsg-for="Invalid booking" data-valmsg-replace="true"
                             style="text-align:left; color:red;"></span>
 
@@ -115,10 +174,10 @@ container.innerHTML = `
             </div>
         </div>
         <div class="footing-container">
-            <button type="button" class="button primary" onclick="window.location.href='/Services/Index';">Torna alla
-                lista</button>
-            <button type="submit" id="btnAvanti" class="button primary g-recaptcha" data-sitekey=""
-                data-callback="onSubmit">Avanti</button>
+            <button type="button" class="button primary" onclick="window.location.href=&#39;/Services/Index&#39;;">Torna
+                alla lista</button>
+            <button type="submit" id="btnAvanti" class="button primary g-recaptcha"
+                data-sitekey="6LdSmG4cAAAAAOarRxGIhehvv4sPgVeF-vRi-Jqb" data-callback="onSubmit">Avanti</button>
         </div>
     </div>
 </form>
