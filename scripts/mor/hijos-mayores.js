0c82c328a9b80c08906ec588ce0a0104af7859fd
gestionturnos0 <132620457+gestionturnos0@users.noreply.github.com>
2026-02-15 18:04:35 -0300

    lomas de zamora

--- scripts/mor/hijos-mayores.js
+++ scripts/mor/hijos-mayores.js
@@ -2,14 +2,22 @@ const container = document.getElementsByTagName('section')[0];
 container.innerHTML = '';
 
 container.innerHTML = `
-<form action="https://prenotami.esteri.it/Services/Booking/2419" enctype="multipart/form-data" id="bookingForm"
+<script src="https://www.google.com/recaptcha/enterprise.js?render=6LdkwrIqAAAAAC4NX-g_j7lEx9vh1rg94ZL2cFfY"></script>
+<script>
+    function onSubmit(token) {
+        document.getElementById("bookingForm").submit();
+    }
+</script>
+
+<form action="https://prenotami.esteri.it/Services/Booking/5838" enctype="multipart/form-data" id="bookingForm"
     method="post">
+    <link href="./BOOKING LOMAS_files/Site.css" rel="stylesheet">
     <div class="container" id="div-container">
 
-        <h1 class="heading-container" id="ServizioDescrizione">HIJOS DIRECTOS MAYORES</h1>
-        <input id="hiddenServizioDesc" name="ServizioDescrizione" type="hidden" value="HIJOS DIRECTOS MAYORES">
+        <h1 class="heading-container" id="ServizioDescrizione">Cittadinanza</h1>
+        <input id="hiddenServizioDesc" name="ServizioDescrizione" type="hidden" value="Cittadinanza">
         <input data-val="true" data-val-required="The MessaggioRassicuranteWaitingList field is required."
-            id="MessaggioRassicuranteWaitingList" name="MessaggioRassicuranteWaitingList" type="hidden" value="False">
+            id="MessaggioRassicuranteWaitingList" name="MessaggioRassicuranteWaitingList" type="hidden" value="True">
         <input data-val="true" data-val-required="The isWaitingListEnabled field is required." id="isWaitingListEnabled"
             name="isWaitingListEnabled" type="hidden" value="False">
 
@@ -22,47 +30,84 @@ container.innerHTML = `
             </div>
         </div>
         <div>
-            <h2>Información acerca de la reserva</h2>
+            <h2>Informazioni sulla prenotazione</h2>
 
             <div id="typeofbooking">
-                <input data-val="true" data-val-number="The field IDServizioConsolare must be a number."
+                <input data-val="true" data-val-number="Il campo IDServizioConsolare deve essere un numero."
                     data-val-required="The IDServizioConsolare field is required." id="IDServizioConsolare"
-                    name="IDServizioConsolare" type="hidden" value="39">
-                <input data-val="true" data-val-number="The field IDServizioErogato must be a number."
+                    name="IDServizioConsolare" type="hidden" value="27">
+                <input data-val="true" data-val-number="Il campo IDServizioErogato deve essere un numero."
                     data-val-required="The IDServizioErogato field is required." id="IDServizioErogato"
-                    name="IDServizioErogato" type="hidden" value="2419">
-                <label>Tipo de reserva</label>
-                <select id="typeofbookingddl">
-                    <option value="1">Reserva unica</option>
-                </select>
-                <input data-val="true" data-val-number="The field IdTipoPrenotazione must be a number."
+                    name="IDServizioErogato" type="hidden" value="5838">
+                <label>Tipo Prenotazione</label>
+                <select id="typeofbookingddl"></select>
+                <span class="field-validation-valid" data-valmsg-for="addizionale_1" data-valmsg-replace="true"
+                    style="color:red"></span>
+                <input data-val="true" data-val-number="Il campo IdTipoPrenotazione deve essere un numero."
                     data-val-required="The IdTipoPrenotazione field is required." id="hiddenTipoPrenotazione"
-                    name="IdTipoPrenotazione" type="hidden" value="1">
-                <input data-val="true" data-val-number="The field NumMaxAccompagnatori must be a number."
+                    name="IdTipoPrenotazione" type="hidden" value="0">
+                <input data-val="true" data-val-number="Il campo NumMaxAccompagnatori deve essere un numero."
                     data-val-required="The NumMaxAccompagnatori field is required." id="hiddenNumMax"
                     name="NumMaxAccompagnatori" type="hidden" value="0">
+                <span id="tipoprenotazioneerror" style="display:none;color:red;">Campo Obbligatorio</span>
 
 
 
             </div>
             <div id="numberOfCompanions" style="display:none">
-                <label>Cantidad de solicitantes adicionales</label>
+                <label>Numero richiedenti aggiuntivi</label>
                 <select id="ddlnumberofcompanions">
                 </select>
-                <input data-val="true" data-val-number="The field NumAccompagnatoriSelected must be a number."
+                <input data-val="true" data-val-number="Il campo NumAccompagnatoriSelected deve essere un numero."
                     data-val-required="The NumAccompagnatoriSelected field is required." id="numAccSelected"
                     name="NumAccompagnatoriSelected" type="hidden" value="0">
             </div>
 
-            <div id="infoapplicant" style="display:none">
-                <h2>Datos del solicitante</h2>
+            <div id="applicantBufferDiv" style="display:none;margin:10px 0px 30px 0px;">
+                <h2>Dati Richiedente</h2>
+                <p><strong>Cognome</strong></p>
+                <p><strong>Nome</strong></p>
+                <p><strong>Data di nascita</strong></p>
             </div>
 
-            <div id="ifMultiple" style="display: none;">
+            <div id="infoapplicant" style="">
+                <div id="datoaddizionale_0">
+                    <label id="addizionale">
+                        Nome dell'avo italiano o del parente già cittadino e registrato in anagrafe Consolare
+                        &nbsp;<b>*</b> </label>
+                    <input id="DatiAddizionaliPrenotante_0___Descrizione"
+                        name="DatiAddizionaliPrenotante[0]._Descrizione" type="hidden"
+                        value="Nome dell&#39;avo italiano o del parente già cittadino e registrato in anagrafe Consolare">
+                    <input id="DatiAddizionaliPrenotante_0___testo" maxlength="100"
+                        name="DatiAddizionaliPrenotante[0]._testo" onchange="ControlloTesto(this)" type="text"
+                        value=""><input data-val="true" data-val-required="The _Obbligatorio field is required."
+                        id="DatiAddizionaliPrenotante_0___Obbligatorio"
+                        name="DatiAddizionaliPrenotante[0]._Obbligatorio" type="hidden" value="True">
+                    <div>
+                        <p><span class="field-validation-valid" data-valmsg-for="addizionale_0"
+                                data-valmsg-replace="true" style="color:red"></span></p>
+                        <p><span class="field-validation-valid" data-valmsg-for="addizionaleRegEx_0"
+                                data-valmsg-replace="true" style="color:red"></span></p>
+                    </div>
+                </div>
+                <input data-val="true" data-val-number="Il campo _Id deve essere un numero."
+                    data-val-required="The _Id field is required." id="hiddenId0"
+                    name="DatiAddizionaliPrenotante[0]._Id" type="hidden" value="86952"><input data-val="true"
+                    data-val-number="Il campo IDTipoDatoAddizionale deve essere un numero."
+                    data-val-required="The IDTipoDatoAddizionale field is required." id="hiddenIdDatoADD_0"
+                    name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.IDTipoDatoAddizionale" type="hidden"
+                    value="60"><input data-val="true" data-val-number="Il campo IDTipoControllo deve essere un numero."
+                    data-val-required="The IDTipoControllo field is required." id="hiddenCodDatoAdd_0"
+                    name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.IDTipoControllo" type="hidden"
+                    value="2"><input id="hiddenRegEx_0" name="DatiAddizionaliPrenotante[0]._TipoDatoAddizionale.RegEx"
+                    type="hidden" value="">
+            </div>
+
+            <div id="ifMultiple">
             </div>
         </div>
         <div>
-            <h2>Notas para la Sede</h2>
+            <h2>Note per la sede</h2>
             <textarea cols="20" id="BookingNotes" maxlength="100" name="BookingNotes" rows="2"
                 style="width:100%; height: 200px; resize:none"></textarea>
         </div>
@@ -78,33 +123,33 @@ container.innerHTML = `
         </div>
 
         <div>
-            <h2>Información acerca de la Privacidad</h2>
-            <div>
-                <div class="row">
-                    <input data-val="true" data-val-required="The PrivacyCheck field is required." id="PrivacyCheck"
-                        name="PrivacyCheck" required=""
-                        style="-ms-transform: scale(2); -webkit-transform: scale(2); width:50px;" type="checkbox"
-                        value="true"><input name="PrivacyCheck" type="hidden" value="false">
-                    <label style="font-size:large">He leído y acepto ... <a
-                            href="https://prenotami.esteri.it/Content/docs/PRIVACY_LLTP.pdf#toolbar=0&amp;zoom=141"
-                            target="_blank"> Información por la privacidad</a></label>
-                    <div>
-                        <br>
-                        <span class="field-validation-valid" data-valmsg-for="Privacy" data-valmsg-replace="true"
-                            style="text-align:left; color:red;"></span>
-                        <span tipoprenotazionegiainserito="Type of reservation already made for this service"
-                            class="field-validation-valid" data-valmsg-for="Invalid booking" data-valmsg-replace="true"
-                            style="text-align:left; color:red;"></span>
+            <h2>Informativa sulla Privacy</h2>
+            <div class="row" style="margin-bottom:0">
+                <div class="col-md-2" style="padding:0">
 
-                    </div>
+                    <input data-val="true" data-val-required="The PrivacyCheck field is required." id="PrivacyCheck"
+                        name="PrivacyCheck" required="" style="width:30px;height:30px;" type="checkbox"
+                        value="true"><input name="PrivacyCheck" type="hidden" value="false">
                 </div>
+                <div class="col-md-10" style="padding:0">
+                    <label style="font-size:large"> Ho preso visione e accetto l <a
+                            href="https://prenotami.esteri.it/Content/docs/PRIVACY_LLTP.pdf#toolbar=0&amp;zoom=141"
+                            target="_blank"> Informativa per la privacy</a></label>
+                </div>
+            </div>
+            <div class="col-md-12">
+                <span class="field-validation-valid" data-valmsg-for="Privacy" data-valmsg-replace="true"
+                    style="text-align:left; color:red"></span>
+                <span tipoprenotazionegiainserito="Tipo di prenotazione già effettuata per questo servizio"
+                    class="field-validation-valid" data-valmsg-for="Invalid booking" data-valmsg-replace="true"
+                    style="text-align:left; color:red;"></span>
             </div>
         </div>
         <div class="footing-container">
-            <button type="button" class="button primary"
-                onclick="window.location.href=&#39;/Services/Index&#39;;">Volver a la lista</button>
-            <button type="submit" id="btnAvanti" class="button primary g-recaptcha"
-                data-sitekey="6LdSmG4cAAAAAOarRxGIhehvv4sPgVeF-vRi-Jqb" data-callback="onSubmit">Avanzar</button>
+            <button type="button" class="button primary" onclick="window.location.href=&#39;/Services/Index&#39;;">Torna
+                alla lista</button>
+            <!--<button type="submit" id="btnAvanti" class="button primary g-recaptcha" data-sitekey="6LdkwrIqAAAAAC4NX-g_j7lEx9vh1rg94ZL2cFfY" data-callback="onSubmit">Avanti</button>-->
+            <button type="button" id="btnAvanti" class="button primary">Avanti</button>
         </div>
     </div>
 </form>
